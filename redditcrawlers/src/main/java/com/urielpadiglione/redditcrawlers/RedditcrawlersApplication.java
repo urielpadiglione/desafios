4c62f8db31307cd07314fa459601e717c328758a
urielpadiglione <47230450+urielpadiglione@users.noreply.github.com>
2019-02-02 14:50:27 -0200

    Feito o crawler que le os subreddits colocado em parametro

--- redditcrawlers/src/main/java/com/urielpadiglione/redditcrawlers/RedditcrawlersApplication.java
+++ redditcrawlers/src/main/java/com/urielpadiglione/redditcrawlers/RedditcrawlersApplication.java
@@ -1,13 +1,30 @@
 package com.urielpadiglione.redditcrawlers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RedditcrawlersApplication {
+import com.urielpadiglione.redditcrawlers.component.CrawlerComponent;
+import com.urielpadiglione.redditcrawlers.service.CrawlerService;
 
+@SpringBootApplication
+public class RedditcrawlersApplication implements CommandLineRunner{
+
+	@Autowired
+	CrawlerComponent crawlerComponent;
+	
+	@Autowired
+	CrawlerService crawlerService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RedditcrawlersApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		crawlerService.getData(crawlerComponent.getTags());
+		
 	}
 
 }
--- redditcrawlers/src/main/java/com/urielpadiglione/redditcrawlers/service/CrawlerService.java
+++ redditcrawlers/src/main/java/com/urielpadiglione/redditcrawlers/service/CrawlerService.java
@@ -1,5 +1,66 @@
 package com.urielpadiglione.redditcrawlers.service;
 
-public class CrawlerService {
+import java.io.IOException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrawlerService {
+	Logger logger = LoggerFactory.getLogger(CrawlerService.class);
+
+	public void getData(String tags) {
+		String[] subreddits = tags.split(";");
+		String threadLink="";
+		String commentsLink="";
+		String threadName="";
+	
+		try {
+			String url = "https://old.reddit.com";
+			
+			for(String sub : subreddits) {
+
+				Document doc = Jsoup.connect(url+"/r/"+sub).get();
+				Elements posts = doc.select("div[class*=\"thing id-\"]");
+
+				for(Element el : posts) {
+					//pega upvotes e garante que não esteja vazio
+					if(!el.getElementsByClass("score unvoted").attr("title").toString().equals("")) {
+						//pega upvotes
+						Integer votes = Integer.valueOf(el.getElementsByClass("score unvoted").attr("title").toString());
+						
+						if(votes>=5000) {
+							//pega link dos comentários
+							commentsLink = el.select("[class*=\"bylink comments may-blank\"]").attr("href").toString();
+							
+							//pega nome da thread
+							threadName = el.select("[class*=\"title may-blank\"]").text();
+							
+							//pega o link do conteudo quando for externo
+							threadLink = el.getElementsByClass("thumbnail invisible-when-pinned may-blank outbound")
+									.attr("href").toString();
+							
+							logger.info("Subreddit: "+sub);
+							logger.info("Votes:"+votes);
+							logger.info("Name: "+threadName);
+							logger.info("Comments Link:"+commentsLink);
+							logger.info("External link: " +threadLink+"\n");
+							
+						}
+
+					}
+
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
